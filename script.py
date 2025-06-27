92e624860df6766cede155a70edddcc34203912d
jbarber877 <80484728+jbarber877@users.noreply.github.com>
2025-06-27 09:09:49 -0400

    Update script.py

--- script.py
+++ script.py
@@ -9,24 +9,23 @@ def convert(network_type, z1_input, z2_input, z3_input, balanced):
     result = ""
 
     if balanced:
-        avg = (z1 + z2 + z3) / 3
-        if network_type == "delta":
-            y_val = avg / 3
+        // z1 = z2 = z3
+        if network_type == "delta": // convert to wye
+            y_val = z1/3
             result += f"Balanced Δ to Y:\nY1 = Y2 = Y3 = {y_val:.3f}"
         else:
-            delta_val = 3 * avg
+            delta_val = 3 * z1
             result += f"Balanced Y to Δ:\nZab = Zbc = Zca = {delta_val:.3f}"
     else:
-        if network_type == "delta":
-            sum_all = z1 + z2 + z3
-            y1 = (z2 * z3) / sum_all
-            y2 = (z1 * z3) / sum_all
-            y3 = (z1 * z2) / sum_all
+        if network_type == "delta": // convertto wye
+            y1 = (z2 * z3) / (z1 + z2 + z3)
+            y2 = (z1 * z3) / (z1 + z2 + z3)
+            y3 = (z1 * z2) / (z1 + z2 + z3)
             result += f"Δ to Y Conversion:\nY1 = {y1:.3f}\nY2 = {y2:.3f}\nY3 = {y3:.3f}"
-        else:
-            Zab = (z1 + z2 + ((z1 * z2) / z3))
-            Zbc = (z2 + z3 + ((z2 * z3) / z1))
-            Zca = (z3 + z1 + ((z3 * z1) / z2))
+        else: // netwrok is wye type, convert to delta
+            Zab = (z1*z2 + z2*z3 + z3*z1)/z1
+            Zbc = (z1*z2 + z2*z3 + z3*z1)/z2
+            Zca = (z1*z2 + z2*z3 + z3*z1)/z3
             result += f"Y to Δ Conversion:\nZab = {Zab:.3f}\nZbc = {Zbc:.3f}\nZca = {Zca:.3f}"
 
     return result
